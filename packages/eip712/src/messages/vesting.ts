f957673841af0bc5b7af484eb41b6bca98ac2fc6
Tien Nam Dao <tien.dao2@tiki.vn>
2022-07-01 16:31:06 +0700

    fix: missing commit

--- packages/eip712/src/messages/vesting.ts
+++ packages/eip712/src/messages/vesting.ts
@@ -2,9 +2,9 @@ export const MSG_VESTING_TYPES = {
   MsgValue: [
     { name: 'from_address', type: 'string' },
     { name: 'to_address', type: 'string' },
-    { name: 'start_time', type: 'Timestamp' },
+    { name: 'start_time', type: 'string' },
     { name: 'vesting_periods', type: 'Period[]' },
-    { name: 'merge', type: 'bool' },
+    // { name: 'merge', type: 'bool' },
     { name: 'lockup_periods', type: 'Period[]' },
   ],
   Timestamp: [
@@ -29,23 +29,24 @@ export function createMsgVesting(
   //   length: string
   //   amount: { denom: string; amount: string }[]
   // }[],
-  merge: boolean = false,
+  // merge: boolean = false,
 ) {
   const time = new Date(startTime).getTime()
   const tp = {
     nanos: (time % 1000) * 1e6,
     seconds: Math.round(time / 1000),
   }
+  const tp1 = new Date(tp.nanos / 1000 + tp.seconds * 1000).toISOString()
 
   return {
-    type: 'evmos.vesting.v1.MsgCreateClawbackVestingAccount',
+    type: 'astra.vesting.v1.MsgCreateClawbackVestingAccount',
     value: {
       from_address: fromAddress,
       to_address: toAddress,
-      start_time: tp,
+      start_time: tp1,
       lockup_periods: [],
       vesting_periods: vestingPeriods,
-      merge,
+      // merge,
     },
   }
 }
